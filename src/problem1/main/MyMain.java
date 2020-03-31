235ee0f28fd86292c63dc89aa29bb8ea90f73bdb
aryan-dubey <62941748+aryan-dubey@users.noreply.github.com>
2020-03-31 18:09:06 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
-// executable class
+// executable classimport problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(55);
+        binarySearchTree.insert(60);
+
+        System.out.println("Left children of the binary search tree: ");
+        binarySearchTree.traverse();
+        System.out.println("No.of Parents not having their left child are:");
+        System.out.println(binarySearchTree.getCountRight());
+
+    }
 }
+
